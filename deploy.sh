70e9872900fc14da6786b49006e5ebb8f283ffaa
David Crean <david.crean@tealium.com>
2022-08-02 10:11:53 -0400

    clean up

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd ../dist
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:killsunday/fumble-dice.git main:gh-pages
+
+cd -
